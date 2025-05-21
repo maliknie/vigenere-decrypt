4f742f54b1112f20011326e2087b3e0101a92a2a
Malik <malik_2005@icloud.com>
2025-05-21 12:43:34 +0200

    Initial WPF project setup for Vigenère decryptor

---
+++ App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace vigenere_decrypt;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
